bc1c115b5152dc6818dd48494f75a37e5eb73dd5
saurabh <saurabhkb123@gmail.com>
2014-02-24 19:30:58 +0530

    added language detection, text and html checks for content type

--- spyder/middleware.py
+++ spyder/middleware.py
@@ -17,28 +17,40 @@ class RequestsLimiter(object):
 		self.DOMAIN_SET = "DOMAIN_SET"
 
 	def process_request(self, request, spider):
-		domain = urlparse(request.url).hostname
-		if int(self.r.get(self.DOMAIN + ":" + domain) or 0) < self.LIMIT:
-			self.r.sadd(self.DOMAIN_SET, domain)
-			self.r.incr(self.DOMAIN + ":" + domain, 1)
-			return None
-		else:
-			log.msg(request.url, level=log.WARNING)
+		try:
+			domain = urlparse(request.url).hostname
+			if int(self.r.get(self.DOMAIN + ":" + domain) or 0) < self.LIMIT:
+				self.r.sadd(self.DOMAIN_SET, domain)
+				self.r.incr(self.DOMAIN + ":" + domain, 1)
+				return None
+			else:
+				log.msg("DOMAIN limit Crossed:%s" % request.url, level=log.CRITICAL)
+				raise IgnoreRequest
+		except TypeError as e:
 			raise IgnoreRequest
+
 
 	def process_response(self, request, response, spider):
-		if 'text/html' not in response.headers['Content-Type'] and 'text/plain' not in response.headers['Content-Type']:
-			log.msg(vars(request), level=log.WARNING)
+		try:
+
+			if 'text/html' not in response.headers['Content-Type'] and 'text/plain' not in response.headers['Content-Type']:
+				log.msg("Non-HTML/Plain:%s" % request.url, level=log.CRITICAL)
+				raise IgnoreRequest
+
+			if langid.classify(response.body)[0] != 'en':
+				log.msg("Non-English:%s" % request.url, level=log.CRITICAL)
+				raise IgnoreRequest
+		except KeyError:
+			log.msg("KeyError(Content-Type):%s" % request.url, level=log.CRITICAL)
 			raise IgnoreRequest
 
-		if langid.classify(response.body)[0] != 'en':
-			raise IgnoreRequest
-
+		del request
 		return response
-
+'''
 class ProxyMiddleware(object):
 	def process_request(self, request, spider):
 		request.meta['proxy'] = "http://10.1.101.150:3128"
 		proxy_user_pass = "111301014:Test_123"
 		encoded_user_pass = base64.encodestring(proxy_user_pass)
 		request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
+'''
---
+++ spyder/nofilter.py
@@ -0,0 +1,30 @@
+from scrapy import log
+import os, sys
+sys.path.append(os.path.abspath('../'))
+from datastore import Datastore
+from scrapy.utils.job import job_dir
+from scrapy.dupefilter import BaseDupeFilter
+from pyhashxx import hashxx
+
+class NoFilter(BaseDupeFilter):
+	"""Request Fingerprint duplicates filter"""
+
+	def __init__(self, path=None):
+		self.r = Datastore()
+		self.URL2ID = "URL2ID"
+
+	@classmethod
+	def from_settings(cls, settings):
+		return cls(job_dir(settings))
+
+	def request_seen(self, request):
+		print "filter:", request.url
+		uid = self.r.get("%s:%s" % (self.URL2ID, hashxx(request.url)))
+		if not uid or int(uid) > 0:
+			pass
+		else:
+			log.msg("FILTER SEEN:%s" % request.url, level = log.CRITICAL)
+			return True
+
+	def close(self, reason):
+		pass
